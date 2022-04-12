be3e26df6da584fda83cfd9ef5230fb2ec6b6fb3
Mihail Gorodovikov <gorodovikov@i-teco.ru>
2022-04-12 23:46:11 +0300

    ex8 fully completed

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
         Client client = new Client();
-        client.pay(100); //дефолтный метод абстрактного класса Person
-        client.pay(); //переопределенный метод абстрактного класса Person
+
+        //дефолтный метод абстрактного класса Person
+        client.pay(100);
+
+        //переопределенный метод абстрактного класса Person
+        client.pay();
+
+        //переопределенный метод из интерфейса Printable
         client.print("Игорь");
+
+        //вывод переменной из абстрактного класса Person через дочерний класс Client
+        System.out.println(client.getStr());
         System.out.println();
 
         Employee employee = new Employee();
-        employee.pay(); //переопределенный метод абстрактного класса Person
+
+        //переопределенный метод абстрактного класса Person
+        employee.pay();
+
+        //переопределенный метод из интерфейса Printable
         client.print("Владимир");
         System.out.println();
 
-
-        SeasonHandler.whichSeason(Season.Winter.getSeasonName(), Season.Winter.getSeasonColor()); //Реализация SeasonHandler + enum
+        //реализация SeasonHandler + enum
+        SeasonHandler.whichSeason(Season.Winter.getSeasonName(), Season.Winter.getSeasonColor());
     }
 
 }
--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,10 @@ public abstract class Person implements Printable, SeasonHandler{
 
     private String str = "Строка";
 
+    public String getStr() {
+        return str;
+    }
+
     public void pay(int sum) {
         System.out.println("Выплаченная сумма " + sum);
     }
