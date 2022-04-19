0d39d4885badf2073c7d4cf63f3483c5ef70e8e5
Mihail Gorodovikov <gorodovikov@i-teco.ru>
2022-04-19 15:07:50 +0300

    delete previous changes

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Account account = new Account("Max1", "asd123");
-
-
-        User<Integer> user = new User<>(13, "Max", 23, account);
-
-        Consumer<User<Integer>> consumer = new Consumer<>("3", "asdf");
-
-        consumer.user = user;
-        consumer.user = new SubUser<>(13, "Max", 23, account);
-
-        System.out.println(print(user));
-
-
+        
     }
 
-    private static <T> String print(T obj) {
-        return obj.toString();
-    }
 }
