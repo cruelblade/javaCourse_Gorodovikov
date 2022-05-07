c6be2716b92a6becaf639ef7b54ce5332805bc80
Mihail Gorodovikov <gorodovikov@i-teco.ru>
2022-05-08 00:29:52 +0300

    ex12 eventually done with LocalDate

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.function.*;
 
@@ -11,14 +13,24 @@ public class Main {
         System.out.println("Введите год рождения студента");
         int birthYear1 = scanner.nextInt();
 
-        Predicate<Integer> predicate = birthYear -> birthYear < 1995;
+        LocalDate current_date = LocalDate.now();
+        int age1 = current_date.getYear() - birthYear1;
+
+        Predicate<Integer> predicateAge = age -> age >= 17 & age <= 40;
+
+        if (!predicateAge.test(age1)) {
+            System.out.println("Возраст студента должен быть от 17 до 40");
+            System.exit(0);
+        }
+
+        Predicate<Integer> predicateBirthYear = birthYear -> birthYear < 1995;
 
         Function<Person, Student> convert;
 
-        if(predicate.test(birthYear1)) {
-            convert = person -> new Student("11", 11, "УБИН-01-22");
+        if (predicateBirthYear.test(birthYear1)) {
+            convert = person -> new Student("11", 11, "УбИН-01-22");
         } else {
-            convert = person -> new Student("11", 11, "УБИН-02-22");
+            convert = person -> new Student("11", 11, "УбИН-02-22");
         }
 
         Student student = convert.apply(person);
