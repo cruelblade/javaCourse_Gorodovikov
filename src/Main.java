3b6e4e5cb1086a5cd6aae5587d05331bff2c475a
Mihail Gorodovikov <gorodovikov@i-teco.ru>
2022-05-22 02:46:41 +0300

    ex14 done

--- src/Main.java
+++ src/Main.java
@@ -2,56 +2,60 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) {
 
+        List<Person> arrayList = new ArrayList<>(Arrays.asList(
+                new Person("Ben", 53), new Person("Mei", 58), new Person("Peter", 17), new Person("Mary", 17)));
 
-        List<Person> people = setList();
+        //сортировка по возрастанию
+        arrayList.stream().sorted(Comparator.comparing(Person::getAge)).forEach(System.out::println);
+        System.out.println();
 
-//        System.out.println(people.stream().allMatch(person -> !person.getName().equals("ROMAN")));
-//        System.out.println(people.stream().anyMatch(person -> person.getAge().equals(22)));
-//        System.out.println(people.stream().noneMatch(person -> person.getName().equals("Ivan")));
+        //сортировка по убыванию
+        arrayList.stream().sorted(Comparator.comparing(Person::getAge).reversed()).forEach(System.out::println);
+        System.out.println();
 
-        System.out.println(people.stream().reduce(0, (a, b) -> {
-            if (b.getAge() >= 18)
-                return a + b.getAge();
-            else
-                return a;
-        }, Integer::sum));
+        //фильтрация по полю
+        arrayList.stream().filter(s -> s.getAge() < 18).forEach(System.out::println);
+        System.out.println();
+
+        //группировка
+        Map <Integer, List<Person>> groupingByAge =
+                arrayList.stream().sorted(Comparator.comparing(Person::getAge)).collect(Collectors.groupingBy(Person::getAge));
+
+        groupingByAge.forEach((age, person) -> {
+            System.out.print("Age: " + age);
+            person.forEach(e -> System.out.print("; " + e));
+            System.out.println();
+        });
+        System.out.println();
+
+        //минимальный объект
+        arrayList.stream().min(Comparator.comparing(Person::getAge).thenComparing(Person::getName)).ifPresent(System.out::println);
+        System.out.println();
+
+        //максимальный объект
+        arrayList.stream().max(Comparator.comparing(Person::getAge)).ifPresent(System.out::println);
+        System.out.println();
+
+        //allMatch
+        if (arrayList.stream().allMatch(e -> e.getAge() > 16))
+            System.out.println("Всем больше 16");
+        System.out.println();
+
+        //anyMatch
+        if(arrayList.stream().anyMatch(e -> e.getName().equals("Peter")))
+            System.out.println("Там есть Петр");
+        System.out.println();
+
+        //noneMatch
+        if(arrayList.stream().noneMatch(e -> e.getName().equals("Ivan")))
+            System.out.println("Ивана тут нет");
+        System.out.println();
 
 
-
-        //разбор optional и comparator (дальнейший с ifPresent разбор ~ на 50 минуте)
-//        PersonComparator pc = new PersonComparator();
-
-//        Map<Integer, List<Person>> groupingByAge =
-//                people.stream().sorted(Comparator.comparing(Person::getName)).
-//                        collect(Collectors.groupingBy(Person::getAge));
-//
-//        groupingByAge.forEach((age, person) -> {
-//            System.out.print("Age: " + age);
-//            person.forEach(e -> System.out.print("; " + e));
-//            System.out.println();
-//        });
-//        System.out.println();
-//
-//        Optional<List<Person>> listOptional = Optional.ofNullable(null);
-//        List<Person> list = listOptional.orElse(setList());
-//        list.forEach(System.out::println);
     }
 
-    private static List<Person> setList() {
-        return Arrays.asList(new Person("Max", 12),
-                new Person("Anton", 22),
-                new Person("Ivan", 24),
-                new Person("Alexey", 16));
-    }
 
 }
-
-//class PersonComparator implements Comparator<Person> {
-//
-//    @Override
-//    public int compare(Person o1, Person o2) {
-//        return o1.getAge().compareTo(o2.getAge());
-//    }
-//}
